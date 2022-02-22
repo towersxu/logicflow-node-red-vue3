cb6838a11e208ea7e7aed854628addaf242d5d41
xutao <987427795@qq.com>
2022-02-22 16:32:56 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    11
+    <!-- <LogicFlow /> -->
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import LogicFlow from "./components/LogicFlow.vue";
+
+export default defineComponent({
+  name: "App",
+  components: {
+    LogicFlow,
+  },
+});
+</script>
---
+++ src/components/LogicFlow.vue
@@ -0,0 +1,28 @@
+<template>
+  <div ref="container">11</div>
+</template>
+
+<script>
+import LogicFlow from "@logicflow/core";
+
+export default {
+  name: "LogicFlow",
+  props: {
+    msg: String,
+  },
+  mounted() {
+    // this.lf = new LogicFlow({
+    //   container: this.$refs.container,
+    //   width: 500,
+    //   height: 500,
+    //   grid: {
+    //     visible: true,
+    //     size: 20,
+    //   },
+    // });
+    // this.lf.render();
+  },
+};
+</script>
+<style scoped>
+</style>
